a85a540e2b5554da626c664665722aefe4d44cfd
Prasannakumar <prasannakumar999>
2022-05-12 21:38:22 +0530

    firstreact

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import FunctionalComponent from './src1/classcomp';
+import ClassComponent from './src1/functionalcomponent';
+import './src1/sty.css'
+import { useState } from 'react';
 function App() {
-  return (
+  const[name1,setName1]=useState(false);
+  const[name2,setName2]=useState(false);
+
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{fontSize:"3vw"}}>Style Using Function Component and Class Component</h1>
+      <div className="hid">
+      <header1>  
+      <button onClick={()=>{name1?setName1(false):setName1(true)}}>To Style with Functional Component</button>
+      {name1&&<FunctionalComponent />}
+      </header1>
+
+      <header2>
+      <button onClick={()=>{name2?setName2(false):setName2(true)}} >To Style with Class Component</button>
+      {name2&&<ClassComponent />}
+      </header2>
+      </div>
     </div>
   );
 }
